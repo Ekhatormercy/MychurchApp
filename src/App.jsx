d65aee7711ba902de5e2c480318f446ced75a5d2
Ekhatormercy <ekhatormercy07@gmail.com>
2024-01-24 16:33:26 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Login from "./Components/Onboarding/login"
+import LogIn from "./Components/Onboarding/login"
 import SignUp from "./Components/Onboarding/signUp"
 import Attendance from './Components/MemberAttendance/Attendance'
 import Payment from './Components/ViewPayment/Viewpayment'
